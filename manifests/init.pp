3a0fa4762afe74ba12aa76a7aa1cb2e74d1d8c11
simpsonjulian <simpsonjulian@gmail.com>
2009-09-29 21:32:31 +0100

    error

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,8 @@ class common::ubuntu {
   exec { 
     "apt get update":
       command => "/usr/bin/apt-get update";
-    "apt get upgrade":
-      command => "/usr/bin/aptitide upgrade";
+    #"apt get upgrade":
+    #  command => "/usr/bin/aptitude safe-upgrade";
   }
 
 
