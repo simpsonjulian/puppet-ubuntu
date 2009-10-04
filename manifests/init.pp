446cfb0b656328fd10846ad239550a4a4f4cf0cb
simpsonjulian <simpsonjulian@gmail.com>
2009-10-04 21:29:52 +0100

    add a preseed defo

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,21 @@ class common::ubuntu {
   exec { 
     "apt get update":
       command => "/usr/bin/apt-get update";
-    #"apt get upgrade":
-    #  command => "/usr/bin/aptitude safe-upgrade";
   }
 
+}
+define preseed_package ( $ensure, $module) {
+
+  file { "/var/local/preseed/$name.preseed":
+    source => "puppet:///${module}/var/local/preseed/$name.preseed",
+    mode => 600,
+    backup => false,
+  }
+
+  package { "$name":
+    ensure => $ensure,
+    responsefile => "/var/local/preseed/$name.preseed",
+    require => File["/var/local/preseed/$name.preseed"],
+  }
 
 }
